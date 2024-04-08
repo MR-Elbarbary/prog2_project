d9f12eb0b4d14d0458909a9f46a25bd36c266504
MR-Elbarbary <121816435+MR-Elbarbary@users.noreply.github.com>
2024-04-08 22:04:13 +0200

    fixed a small bug

--- airline/src/main/java/airline/App.java
+++ airline/src/main/java/airline/App.java
@@ -1,5 +1,10 @@
-package java.airline;
+package airline;
 
-public class app {
+public class App {
+
+    public static void setRoot(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setRoot'");
+    }
     
 }
