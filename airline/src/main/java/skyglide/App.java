c17a06b9b6d9a910866e895923fd015843b4ee49
MR-Elbarbary <121816435+MR-Elbarbary@users.noreply.github.com>
2024-04-17 01:04:23 +0200

    added a tittle "SkyGlide"

--- airline/src/main/java/skyglide/App.java
+++ airline/src/main/java/skyglide/App.java
@@ -19,6 +19,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("Scene1"), 640, 480);
         stage.setScene(scene);
+        stage.setTitle("SkyGlide");
         stage.show();
     }
 
